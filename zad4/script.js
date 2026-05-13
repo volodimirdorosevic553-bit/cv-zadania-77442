fba528ad18993061f13956b5e45e85668e09ecac
volodimirdorosevic553-bit <volodimirdorosevic553@gmail.com>
2026-05-13 16:16:07 +0200

    Delete zad4 directory

--- zad4/script.js
+++
@@ -1,2 +0,0 @@
-// 77442 Volodymyr Doroshevych - zad4
-const themeBtn=document.querySelector('#themeBtn');const toggleBtn=document.querySelector('#toggleProjects');const projects=document.querySelector('#projects');themeBtn.addEventListener('click',()=>document.body.classList.toggle('red'));toggleBtn.addEventListener('click',()=>projects.classList.toggle('hidden'));
