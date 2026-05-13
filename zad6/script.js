b6bfc129954ab7e0d8b320e6db56172b52395e22
volodimirdorosevic553-bit <volodimirdorosevic553@gmail.com>
2026-05-13 16:16:33 +0200

    Delete zad6 directory

--- zad6/script.js
+++
@@ -1,2 +0,0 @@
-// 77442 Volodymyr Doroshevych - zad6
-fetch('data.json').then(r=>r.json()).then(data=>{skills.innerHTML=data.skills.map(s=>`<li>${s}</li>`).join('');projects.innerHTML=data.projects.map(p=>`<div class="card"><h4>${p.name}</h4><p>${p.description}</p></div>`).join('');}).catch(()=>{projects.innerHTML='<p class="error">Nie udało się pobrać danych JSON.</p>'});
